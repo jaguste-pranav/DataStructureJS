888291f30207a673a84a057ca30b9cf72077f286
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-19 02:35:05 +0530

    [pranav] Add. All the problem statements regarding Maps

---
+++ Map-Dice.js
@@ -0,0 +1,31 @@
+//Find the max number times the number was repeated
+var diceDict = new Object();
+var diceDict = {};
+let count=0;
+let diceNum=0;
+while(true)
+{
+    if(diceDict[diceNum]>9)
+    {
+        break;
+    }
+    else
+    {
+        diceNum=Math.floor(Math.random() * 6) + 1;
+        if (!diceDict.hasOwnProperty(diceNum)) 
+        {
+            diceDict[diceNum] = 1;
+        }
+        else
+        {
+            diceDict[diceNum]++;
+        }
+    }
+}
+for(var key in diceDict)
+{
+    if(diceDict[key]==10)
+    {
+        console.log(key);
+    }
+}
